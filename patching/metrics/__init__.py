0ca74d56c9fff13253042fc4acccb36cdff52a1d
jerr-it <jrgls.no-reply@gmx.de>
2023-04-17 14:27:59 +0200

    Added pattern variation metric

--- patching/metrics/__init__.py
+++ patching/metrics/__init__.py
@@ -1,10 +1,13 @@
 from patching.metrics.memory import Memory
+from patching.metrics.pattern_variation import PatternVariationGenerated, PatternVariationFixed
 from patching.metrics.runtime import Runtime
 from patching.metrics.tpkl import TPKLGenerated, TPKLPatched
 
 metrics = [
     {"name": "TPKL Generated", "unit": "", "object": TPKLGenerated()},
     {"name": "TPKL Patched", "unit": "", "object": TPKLPatched()},
+    {"name": "Pattern variance generated", "unit": "", "object": PatternVariationGenerated()},
+    {"name": "Pattern variance fixed", "unit": "", "object": PatternVariationFixed()},
     {"name": "Memory", "unit": "B", "object": Memory()},
     {"name": "Runtime", "unit": "s", "object": Runtime()}
 ]
---
+++ patching/metrics/pattern_variation.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from patching.metrics.metric import Metric
+
+
+class PatternVariationGenerated(Metric):
+    def pre_hook(self):
+        pass
+
+    def post_hook(
+            self,
+            original_level: list[str],
+            generated_level: list[str],
+            fixed_level: list[str],
+    ) -> float:
+        return pattern_variation(generated_level)
+
+
+class PatternVariationFixed(Metric):
+    def pre_hook(self):
+        pass
+
+    def post_hook(
+            self,
+            original_level: list[str],
+            generated_level: list[str],
+            fixed_level: list[str],
+    ) -> float:
+        return pattern_variation(fixed_level)
+
+
+def pattern_variation(level: list[str], kernel_size: int = 3) -> float:
+    # Convert level to 2d numpy array, splitting the strings into lists of chars
+    level = np.array([list(x) for x in level])
+
+    # Iterate over all kernel_size x kernel_size tiles in the level
+    # and count the number of times each pattern appears
+    pattern_counts = {}
+    for row in range(level.shape[0] - kernel_size + 1):
+        for col in range(level.shape[1] - kernel_size + 1):
+            pattern = tuple(level[row:row + kernel_size, col:col + kernel_size].flatten())
+            pattern_counts[pattern] = pattern_counts.get(pattern, 0) + 1
+
+    # Calculate the total number of patterns in the level
+    total_patterns = (level.shape[0] - kernel_size + 1) * (level.shape[1] - kernel_size + 1)
+
+    return len(pattern_counts) / total_patterns
