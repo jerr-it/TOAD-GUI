33a71fd9ed97fafb7ff336d796a5b4f8e7c87a4d
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2023-05-22 16:10:04 +0200

    MarioAI Framework now tracks entire path the agent takes, changed difficulty

--- patching/metrics/difficulty.py
+++ patching/metrics/difficulty.py
@@ -50,97 +50,68 @@ def difficulty(level: list[str], mario_result: py4j.java_gateway.JavaObject) -> 
     :param mario_result: Data gathered by the AI agent, type is MarioResult (see Mario AI Framework)
     :return: Difficulty score, higher meaning more difficult
     """
-    # Static evaluation
-
     # Convert level to numpy ndarray
     level = np.array([list(row) for row in level])
 
     # Determine the height for every vertical column
     width: int = level.shape[1]
-    heights: list[int] = [0] * width
+    height: int = level.shape[0]
 
-    for column in range(width):
-        # Move down the column until we find a non "-" character
-        for row in range(level.shape[0]):
-            if level[row][column] != "-":
-                heights[column] = level.shape[0] - row
-                break
-
-    # Iterate all vertical columns
-    current_gap_width: int = 0
-
-    static_gap_widths = []
     enemy_count: int = 0
     cannon_count: int = 0
     tube_count: int = 0
     powerup_count: int = 0
 
-    for column in range(width - 1):
-        if heights[column] == 0:
+    path = mario_result.getMarioPath()  # ArrayList<MarioPosition>
+
+    current_gap_width = 0
+    jumps: list[int] = []  # List of gap lengths mario jumped over
+    covered_x_position = 0
+    for position in path:
+        x_pos = position.getX()
+        y_pos = position.getY()
+
+        if x_pos <= covered_x_position:
+            continue
+
+        is_gap = True
+        for y in range(y_pos, height):
+            if level[y][x_pos] not in NON_BLOCKS:
+                is_gap = False
+                break
+
+        covered_x_position += 1
+
+        level[y_pos][x_pos] = MARIO_PATH_TOKEN
+
+        if is_gap:
             current_gap_width += 1
         else:
             if current_gap_width > 0:
-                static_gap_widths.append(current_gap_width)
+                jumps.append(current_gap_width)
                 current_gap_width = 0
 
+    easy_jumps = 0
+    difficult_jumps = 0
+    for jump in jumps:
+        if jump <= 6:
+            easy_jumps += 1
+        else:
+            difficult_jumps += 1
+
+    for column in range(width - 1):
         enemy_count += count_enemies(level[:, column])
         cannon_count += count_cannon(level[:, column])
         tube_count += count_tubes((level[:, column], level[:, column + 1]))
         powerup_count += count_powerups(level[:, column])
 
-    # Dynamic evaluation
-
-    # Find the gaps mario *actually* jumped over
-    # 10 is the maximum gap width mario can handle
-    # 5 is the highest mario can jump
-    jumps = mario_result.getJumps()
-    dynamic_gap_widths = []
-    for jump in jumps:
-        y_start = int(jump.getStart().getMarioY() / 16)
-        y_end = int(jump.getEnd().getMarioY() / 16) - 1
-
-        y_pos = y_start - 3 if y_start > y_end else y_end - 5
-
-        x_start = max(int(jump.getStart().getMarioX() / 16) - 1, 0)
-        x_end = min(int(jump.getEnd().getMarioX() / 16) + 1, width)
-
-        # Check downwards from all mario positions during the jump
-        # to check if he's jumping an actual gap
-        gap_length = 0
-        for x in range(x_start, x_end):
-            is_gap = True
-            for y in range(y_pos, level.shape[0]):
-                if level[y][x] not in NON_BLOCKS:
-                    is_gap = False
-                    break
-                level[y][x] = "x"
-
-            if is_gap:
-                gap_length += 1
-            else:
-                if gap_length > 0:
-                    dynamic_gap_widths.append(gap_length)
-                    gap_length = 0
-
-    hurts = mario_result.getMarioNumHurts()
-    fall_kills = mario_result.getKillsByFall()
-    collected_powerups = \
-        mario_result.getCurrentLives() \
-        + mario_result.getNumCollectedMushrooms() \
-        + mario_result.getNumCollectedFireflower()
-
-    static_gap_avg = sum(static_gap_widths) / (1 + len(static_gap_widths))
-    dynamic_gap_avg = sum(dynamic_gap_widths) / (1 + len(dynamic_gap_widths))
-
-    score = static_gap_avg * 1.2 \
-        + dynamic_gap_avg * 1.428 \
-        + (enemy_count - hurts - fall_kills) * 0.02 \
-        + fall_kills * 0.333 \
-        + hurts * 1.5 \
-        + tube_count * 0.1 \
-        + cannon_count * 0.333 \
-        - (powerup_count - collected_powerups) * 0.2 \
-        - collected_powerups * 2.0
+    score = \
+        + enemy_count \
+        + cannon_count \
+        + tube_count \
+        - powerup_count \
+        + easy_jumps * 0.5 \
+        + difficult_jumps * 2.0 \
 
     return score
 
