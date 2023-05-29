fa178ca37a67def8b137208cf29aff705742c77e
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2023-05-29 17:38:57 +0200

    Fixed best fit stitching creating infinite loops by picking the same spot repeatedly

--- patching/best_fit_stitching.py
+++ patching/best_fit_stitching.py
@@ -6,6 +6,11 @@ from patching.patcher import Patcher
 
 
 class BestFitStitching(Patcher):
+    def __init__(self):
+        # Keeps track which sections we already tried to avoid infinite loops
+        # level -> list[int]
+        self.record: dict[str, list[int]] = {}
+
     def patch(
             self,
             original_level: list[str],
@@ -13,7 +18,10 @@ class BestFitStitching(Patcher):
             broken_range: tuple[tuple[int, int], tuple[int, int]],  # (x_range, y_range)
             generator_path: str = "",
     ) -> list[str]:
-        # Convert levels to 2d numpy arrays, splitting strings into chars
+        level_key = "@".join(level)
+        if level_key not in self.record:
+            self.record[level_key] = []
+
         original_level = np.array([list(row) for row in original_level])
         level = np.array([list(row) for row in level])
 
@@ -26,6 +34,9 @@ class BestFitStitching(Patcher):
         record_score = -math.inf
         record_pos = -1
         for x in range(1, original_level.shape[1] - x_range_width - 1):
+            if x in self.record[level_key]:
+                continue
+
             original_column_left = original_level[:, x-1]
             original_column_right = original_level[:, x+1]
 
@@ -36,6 +47,8 @@ class BestFitStitching(Patcher):
             if score > record_score:
                 record_score = score
                 record_pos = x
+
+        self.record[level_key].append(record_pos)
 
         repl_range = (
             (record_pos, record_pos + x_range_width),
