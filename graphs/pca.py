94113bd456b99f302f87bd7c7c443e023c2628fe
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2023-06-14 11:38:57 +0200

    Adjusted pca figure

--- graphs/pca.py
+++ graphs/pca.py
@@ -17,7 +17,7 @@ metrics_df["difficulty_change"] = (metrics_df["Difficulty fixed"] - metrics_df["
 metrics_df["Tries"] = metrics_df["Tries"]
 
 y = metrics_df[["patcher"]].copy()
-X = metrics_df[["tpkl_change", "variation_change", "difficulty_change"]].copy()
+X = metrics_df[["tpkl_change", "variation_change", "difficulty_change", "Runtime", "Peak memory", "Tries"]].copy()
 
 x_scaled = StandardScaler().fit_transform(X)
 
@@ -33,12 +33,34 @@ pca_df = pd.DataFrame(
 
 pca_df["target"] = y
 
-with sns.color_palette("Paired"):
-    g = sns.jointplot(
-        data=pca_df,
-        x="PC1",
-        y="PC2",
-        hue="target",
-    )
+fig, axes = plt.subplots(2, 5, figsize=(30, 10), subplot_kw={'aspect': 'equal'})
+fig.suptitle("PCA [Runtime, Memory, Tries, TPKL change, variation change, difficulty change]")
 
-    plt.show()
+colors = sns.color_palette("Paired")
+for idx, patcher in enumerate(["Wave Function Collapse", "Evolutionary Patterns", "Stitching", "Best fit stitching", "Online"]):
+    with sns.color_palette("Paired"):
+        g = sns.scatterplot(
+            data=pca_df.loc[pca_df["target"] == patcher],
+            x="PC1",
+            y="PC2",
+            ax=axes.flatten()[idx],
+            color=colors[idx]
+        )
+        axes.flatten()[idx].set_title(patcher)
+
+for idx, patcher in enumerate(["Wave Function Collapse", "Evolutionary Patterns", "Stitching", "Best fit stitching", "Online"]):
+    with sns.color_palette("Paired"):
+        g = sns.kdeplot(
+            data=pca_df.loc[pca_df["target"] == patcher],
+            x="PC1",
+            y="PC2",
+            ax=axes.flatten()[idx+5],
+            color=colors[idx]
+        )
+
+for ax in axes.flatten():
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+
+plt.tight_layout()
+plt.show()
